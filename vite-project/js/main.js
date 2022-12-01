9e17bea3a2366383d908f0972cc01e93db3b291a
MichelleO3O <113177083+MichelleO3O@users.noreply.github.com>
2022-12-01 09:56:14 -0500

    VITE 3

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,5 +1,5 @@
-import "../styles/style.cssnp";
-import javascriptLogo from "./javascript.svg";
+import "../styles/style.css";
+import javascriptLogo from "../javascript.svg";
 import { setupCounter } from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
