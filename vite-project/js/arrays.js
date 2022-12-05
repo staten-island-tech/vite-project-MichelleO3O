472c94814dd66d0cd85011a4e07a1d3956906cf8
MichelleO3O <113177083+MichelleO3O@users.noreply.github.com>
2022-12-05 10:17:06 -0500

    VITE 5

---
+++ vite-project/js/arrays.js
@@ -0,0 +1,43 @@
+const food = [
+  {
+    name: "Katsudon",
+    price: 15,
+    vegetarian: false,
+  },
+
+  {
+    name: "Choufarci",
+    price: 25,
+    vegetarian: false,
+  },
+
+  {
+    name: "Omurice",
+    price: 10,
+    vegetarian: true,
+  },
+
+  {
+    name: "Takoyaki",
+    price: 5,
+    vegetarian: false,
+  },
+
+  {
+    name: "Crepe",
+    price: 7,
+    vegetarian: true,
+  },
+
+  {
+    name: "Bento Box",
+    price: 15,
+    vegetarian: false,
+  },
+
+  {
+    name: "Pudding",
+    price: 5,
+    vegetarian: true,
+  },
+];
