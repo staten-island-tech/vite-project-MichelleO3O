e9edb2143022974665de720a51f0a0350125da0a
MichelleO3O <113177083+MichelleO3O@users.noreply.github.com>
2022-12-06 10:18:19 -0500

    VITE 6

--- vite-project/js/arrays.js
+++ vite-project/js/arrays.js
@@ -3,41 +3,69 @@ const food = [
     name: "Katsudon",
     price: 15,
     vegetarian: false,
+    img: "https://ninjarecipe.net/wp-content/uploads/2021/12/%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%BC%E3%83%B3%E3%82%B7%E3%83%A7%E3%83%83%E3%83%88-2021-12-03-10.03.29-1024x556.png",
   },
 
   {
     name: "Choufarci",
     price: 25,
     vegetarian: false,
+    img: "https://static.wikia.nocookie.net/shokugekinosoma/images/0/0d/Chou_Farci_%28anime%29.png/revision/latest?cb=20150612205327",
   },
 
   {
     name: "Omurice",
     price: 10,
     vegetarian: true,
+    img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/55660762-c858-4a1b-9239-4e143f216fa4/df4w0j8-1120d82c-06db-4b0a-a433-a264e2d91c8e.png/v1/fill/w_1280,h_720,q_80,strp/anime_rice_omelette_by_trippyhippyjinx_df4w0j8-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvNTU2NjA3NjItYzg1OC00YTFiLTkyMzktNGUxNDNmMjE2ZmE0XC9kZjR3MGo4LTExMjBkODJjLTA2ZGItNGIwYS1hNDMzLWEyNjRlMmQ5MWM4ZS5wbmciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.UYWjMGipphB3iEDYFCvxvm1mddkT4CrzTe40lc_fEbA",
   },
 
   {
     name: "Takoyaki",
     price: 5,
     vegetarian: false,
+    img: "https://itadakimasuanime.files.wordpress.com/2014/12/takoyaki-ookami-shoujo-to-kuro-ouji-08-01.png",
   },
 
   {
     name: "Crepe",
     price: 7,
     vegetarian: true,
+    img: "https://img1.ak.crunchyroll.com/i/spire2/f718d7de6c4b329a5fc71be35d7318421528960814_full.png",
   },
 
   {
     name: "Bento Box",
     price: 15,
     vegetarian: false,
+    img: "https://itadakimasuanime.files.wordpress.com/2014/07/bento-hyouka-15.png",
   },
 
   {
     name: "Pudding",
     price: 5,
     vegetarian: true,
+    img: "https://i.pinimg.com/originals/92/87/08/928708c94c996d37944394d2c5c76da3.png",
+  },
+
+  {
+    name: "Ramen",
+    price: 10,
+    vegetarian: false,
+    img: "https://grapee.jp/en/wp-content/uploads/IMG-6-1.jpg",
+  },
+
+  {
+    name: "Steak",
+    price: 25,
+    vegetarian: false,
+    img: "https://pbs.twimg.com/media/D-FNaZzWsAI6vKP.jpg",
+  },
+
+  {
+    name: "Karage",
+    price: 10,
+    vegetarian: false,
+    img: "https://pbs.twimg.com/media/DLYMbB_W4AEqt1E.jpg",
   },
 ];
