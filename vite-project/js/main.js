3d3929ec3084123b293109a259c972f0d87fb68f
MichelleO3O <mchao9406@gmail.com>
2022-12-21 14:31:27 -0500

    VITE 22

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -33,7 +33,74 @@ document.querySelector(".btn").addEventListener("click", function () {
   }
 });
 
-document.querySelector(".veg-btn").addEventListener("click", function () {});
+document.querySelector(".veg-btn").addEventListener("click", function () {
+  const allCards = document.getElementById("allCards");
+  allCards.innerHTML = "";
+  food
+    .filter((e) => e.vegetarian === true)
+    .forEach((element) => {
+      DOMSelectors.cards.insertAdjacentHTML(
+        "beforeend",
+        `
+      <div class="card">
+        <h2 class="card-title">${element.name}</h2>
+        <img src="${element.img}">
+        <div class="card-details">
+          <h3>$${element.price}</h3>
+          <h4 class="vegetarian">Vegetarian: ${
+            element.vegetarian ? "Yes" : "No"
+          }</h4>
+        </div>
+      </div>
+      `
+      );
+    });
+});
+
+document.querySelector(".non-veg-btn").addEventListener("click", function () {
+  const allCards = document.getElementById("allCards");
+  allCards.innerHTML = "";
+  food
+    .filter((e) => e.vegetarian === false)
+    .forEach((element) => {
+      DOMSelectors.cards.insertAdjacentHTML(
+        "beforeend",
+        `
+      <div class="card">
+        <h2 class="card-title">${element.name}</h2>
+        <img src="${element.img}">
+        <div class="card-details">
+          <h3>$${element.price}</h3>
+          <h4 class="vegetarian">Vegetarian: ${
+            element.vegetarian ? "Yes" : "No"
+          }</h4>
+        </div>
+      </div>
+      `
+      );
+    });
+});
+
+document.querySelector(".all-food").addEventListener("click", function () {
+  const allCards = document.getElementById("allCards");
+  allCards.innerHTML = "";
+  food.forEach((element) => {
+    DOMSelectors.cards.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="card">
+        <h2 class="card-title">${element.name}</h2>
+        <img src="${element.img}">
+        <div class="card-details">
+          <h3>$${element.price}</h3>
+          <h4 class="vegetarian">Vegetarian: ${
+            element.vegetarian ? "Yes" : "No"
+          }</h4>
+        </div>
+      </div>
+      `
+    );
+  });
+});
 
 //inside .forEarch do cards.insertAdjacentHTML to create every card
-//
